90d5b7f8df8435f8b1a19d0006bb054389c02412
gomorizsolt <gomorizsolt@users.noreply.github.com>
2020-06-24 20:02:31 +0000

    deploy: 7fc0e9bbfd326d3d5bfb449aa6c1d01ba26e5cb7

---
+++ precache-manifest.9d7f6a4a0f360f812d600aaf07c6a3d2.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a74f38c2ddaf441fedde8cf7b713467f",
+    "url": "/react-pomodoro/index.html"
+  },
+  {
+    "revision": "c4dabf7449277cbf86c9",
+    "url": "/react-pomodoro/static/css/main.df60703a.chunk.css"
+  },
+  {
+    "revision": "c966a4c8dcaa84b7ccfc",
+    "url": "/react-pomodoro/static/js/2.370c20e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-pomodoro/static/js/2.370c20e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4dabf7449277cbf86c9",
+    "url": "/react-pomodoro/static/js/main.1ec1914f.chunk.js"
+  },
+  {
+    "revision": "2febf34ba0673ae46f1f",
+    "url": "/react-pomodoro/static/js/runtime-main.2e2eaa4f.js"
+  },
+  {
+    "revision": "40be74f0577105990d45f549fb268cec",
+    "url": "/react-pomodoro/static/media/Orbitron-Black.40be74f0.woff2"
+  },
+  {
+    "revision": "42f193b1947684d20db6715cec43b8ce",
+    "url": "/react-pomodoro/static/media/Orbitron-Black.42f193b1.woff"
+  },
+  {
+    "revision": "6be97292694ecc8e34508b8b8d451b67",
+    "url": "/react-pomodoro/static/media/Orbitron-Bold.6be97292.woff2"
+  },
+  {
+    "revision": "9f7967203b6e9ed1b33e4abec47bf3e1",
+    "url": "/react-pomodoro/static/media/Orbitron-Bold.9f796720.woff"
+  },
+  {
+    "revision": "4cec965ea4dea496fc5e18b30784c3fb",
+    "url": "/react-pomodoro/static/media/Orbitron-ExtraBold.4cec965e.woff2"
+  },
+  {
+    "revision": "693d8281c7d8a632bbd3e610c15b1594",
+    "url": "/react-pomodoro/static/media/Orbitron-ExtraBold.693d8281.woff"
+  },
+  {
+    "revision": "120c12a415d626187fa34c5410be4018",
+    "url": "/react-pomodoro/static/media/Orbitron-Medium.120c12a4.woff2"
+  },
+  {
+    "revision": "5da5a58da1b62475573eaaad025e7422",
+    "url": "/react-pomodoro/static/media/Orbitron-Medium.5da5a58d.woff"
+  },
+  {
+    "revision": "2bc781a34a8aeedcd36a0a4de6bfaa78",
+    "url": "/react-pomodoro/static/media/Orbitron-Regular.2bc781a3.woff2"
+  },
+  {
+    "revision": "db6ccf1d106c3c86993581b8e9f5db68",
+    "url": "/react-pomodoro/static/media/Orbitron-Regular.db6ccf1d.woff"
+  },
+  {
+    "revision": "bf8a260e5bd65ed01596683f51308cc9",
+    "url": "/react-pomodoro/static/media/Orbitron-SemiBold.bf8a260e.woff"
+  },
+  {
+    "revision": "e22db750d758536f973f9d855c438f96",
+    "url": "/react-pomodoro/static/media/Orbitron-SemiBold.e22db750.woff2"
+  }
+]);
